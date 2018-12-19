b27eb18862c7e5fc4c6b815dd6595c6a31f0c032
bozsahin <cem.bozsahin@gmail.com>
2018-12-19 19:38:07 +0300

    added MIPS style register-only code gen from TAC

--- examples/expr-tac-symtab/ex-expr.lisp
+++ examples/expr-tac-symtab/ex-expr.lisp
@@ -1,16 +1,54 @@
 ;;;; this example compiles arithmetic expressions and their assignment to 
-;;;; Three address code (TAC)
+;;;; Three address code (TAC) then to register-only ops.
 ;;;; -cem bozsahin
 
+;; Examples in tac and zac directories generate IC code with IC temporaries.
 
-;; This version generates IC names as offsets from a base.
-;; A Symbol table keeps these offsets and type info. Its format is
-;; key: (name blocknon)  value: (type offset)
-;;   type is symbol or number.  Each type takes a fixed amount of space (in bytes, say)
-;;   offset starts with 0, and increases per length required for type.
+;; This version generates TAC with IC temporaries, then creates MIPS-style .data and .text segments
+;; which refer to these temporaries, and implements the TAC as register operations.
+;; This is because in MIPS all ops are done in registers; no op refers to memory locations.
+;; For example (3ac mult a b c) meaning a := b * c in IC code is translated into
+;;  lw $t0,#b
+;;  lw $t1,#c
+;;  mul $t2,$t0,$t1
+;;  sw $t2,#a
+;;
+;; where lw is load word and #x means address of x in its .data block, which we created as a label.
+;; and sw means save the register's value in a word in memory.
+;; $ti are temporary value registers of MIPS-like architectures. All we need is 2 for TAC codes.
+;; We can think of this version as a register-based TAC, rather than true target code.
 
-(defun make-symtab (size)
-  (make-hash-table :test #'equal :size size :rehash-threshold 0.8))  ; we need equal to match a list of values as key
+;; Here is the algorithm: 1) Generate TAC with relative addresses with IC temporaries. 2) Map that result to a 
+;;  register-based TAC with data and code segments. 
+
+;; A Symbol table keeps names (variables and constants) and type info. Its format is
+;; hash key: (name blockno)  hash value: (type value)
+;;   type is NUM or VAR.  Value is relevant for numbers to choose 'li' MIPS command later. Each type takes a fixed amount of 
+;;   space (in MIPS generation i assume one word).
+
+(defun mk-symtab (size)
+  (make-hash-table :test #'equal :size size :rehash-threshold 0.8))  ; we need equal function to match a LIST of values as key
+
+(defparameter *symtab* (mk-symtab 200)) ; this is a global variable, referred to in the grammar's semantic actions
+
+(defparameter *blockno* 0)  ;; increment this everytime a new code block (procedure etc.) is entered. 
+
+(defun target-code-mips (input)
+  (clrhash *symtab*) ; we need to reset the symbol table for every code gen
+  (setf *blockno* 0)
+  (target-code input))
+
+(defun mk-sym-entry (name)
+  "NB: Lisp hash is collision-free, duplicates just replace the older value."
+  (cond ((numberp name) (setf (gethash (list name *blockno*) *symtab*) (list 'num name)))
+	((symbolp name) (setf (gethash (list name *blockno*) *symtab*) (list 'var name)))
+	(t (setf (gethash (list name *blockno*) *symtab*) (list 'unknown name)))))
+
+(defun sym-get-type (val)
+  (first val))
+
+(defun sym-get-value (val)
+  (second val))
 
 ;; SDD section
 ;;
@@ -45,6 +83,63 @@
     (pprint instruction))
   t)
 
+
+(defun create-data-segment ()
+  "only for variables; numbers will use li loading rather than lw
+  If you have more than one block, you need to create .data for each block."
+  (format t "~2%.data~%")
+  (maphash #'(lambda (key val)
+	       (if (equal (sym-get-type val) 'VAR) (format t "~%~A: .word 0" (sym-get-value val))))
+	   *symtab*))
+
+(defun mk-mips (p register)
+  "create li if constant or lw if not"
+  (if (numberp p)
+    (format t "~%li ~A,~A" register p)
+    (format t "~%lw ~A,~A" register p)))
+
+
+(defun mk-mips-3ac (i)
+  (let ((op (first i))
+	(p1 (second i))
+	(p2 (third i))
+	(p3 (fourth i)))
+    (mk-mips p2 "$t0")
+    (mk-mips p3 "$t1")
+    (format t "~%~A $t0,$t0,$t1" op)
+    (format t "~%sw $t0,~A" p1)))
+
+(defun mk-mips-2ac (i)
+  (let ((op (first i))
+	(p1 (second i))
+	(p2 (third i)))
+    (mk-mips p2 "$t1")
+    (format t "~%~A $t0,$zero,$t1" op)
+    (format t "~%sw $t0,~A" p1)))
+
+(defun mk-mips-2copy (i)
+  (let ((p1 (first i))
+	(p2 (second i)))
+    (mk-mips p2 "$t0")
+    (format t "~%sw $t0,~A" p1)))
+
+(defun map-to-mips (code)
+  (create-data-segment)
+  (format t "~2%.text~2%") 
+  (format t "main:~%")
+  (dolist (instruction (second code)) ; NB. code is a grammar variable feature (code i1 i2 i3 ...)
+    (let ((itype (first instruction)))
+      (cond ((equal itype '3AC) (mk-mips-3ac (rest instruction)))
+	    ((equal itype '2AC) (mk-mips-2ac (rest instruction)))
+	    ((equal itype '2COPY) (mk-mips-2copy (rest instruction)))
+	    (t (format t "unknown TAC code: ~A" instruction))))))
+
+(defun tac-to-rac (code)
+  (format t  "~2%TAC code:~2%")
+  (pprint-code code)
+  (format t "~2%MIPS-style code using register ops only:~2%")
+  (map-to-mips code))
+
 ;; some aux functions  to retrieve amd make feature values for grammar variables
 
 (defun mk-place (v)
@@ -69,16 +164,17 @@
   (wrap (list '2copy p1 p2)))
 
 (defun newtemp ()
-  (gensym "t-"))       ; returns a new symbol prefixed t- at run-time
+  (gensym "t-"))       ; returns a new symbol prefixed t- at Lisp run-time
 
 ;;;; LALR data 
 
 (defparameter grammar
 '(
-  (start --> ID COLON EQLS e END  #'(lambda (ID COLON EQLS e END) (pprint-code (mk-code (append (var-get-code e)
+  (start --> ID COLON EQLS e END  #'(lambda (ID COLON EQLS e END) (tac-to-rac (mk-code (append (var-get-code e)
 												(mk-2copy (t-get-val ID)
 													  (var-get-place e)))))))
   (e     --> e ADD te         #'(lambda (e ADD te) (let ((newplace (newtemp)))
+						     (mk-sym-entry newplace)
 						     (list (mk-place newplace)
 							   (mk-code (append (var-get-code e)
 									    (var-get-code te)
@@ -86,6 +182,7 @@
 										    (var-get-place e)
 										    (var-get-place te))))))))
   (e     --> e SUB te         #'(lambda (e SUB te) (let ((newplace (newtemp)))
+						     (mk-sym-entry newplace)
 						     (list (mk-place newplace)
 							   (mk-code (append (var-get-code e)
 									    (var-get-code te)
@@ -94,13 +191,15 @@
 										    (var-get-place te))))))))
   (e     --> te               #'(lambda (te) (identity te)))
   (te    --> te MULT f        #'(lambda (te MULT f) (let ((newplace (newtemp)))
+						     (mk-sym-entry newplace)
 						     (list (mk-place newplace)
 							   (mk-code (append (var-get-code te)
 									    (var-get-code f)
-									    (mk-3ac 'mult newplace
+									    (mk-3ac 'mul newplace
 										    (var-get-place te)
 										    (var-get-place f))))))))
   (te    --> te DIV f         #'(lambda (te DIV f) (let ((newplace (newtemp)))
+						     (mk-sym-entry newplace)
 						     (list (mk-place newplace)
 							   (mk-code (append (var-get-code te)
 									    (var-get-code f)
@@ -110,11 +209,14 @@
   (te    --> f                #'(lambda (f)(identity f)))
   (f     --> LP e RP          #'(lambda (LP e RP) (identity e)))
   (f     --> SUB ID           #'(lambda (SUB ID) (let ((newplace (newtemp)))
+						   (mk-sym-entry newplace)
 						   (list (mk-place newplace)
-							 (mk-code (mk-2ac 'uminus newplace
+							 (mk-code (mk-2ac 'sub newplace
 										  (t-get-val ID)))))))
-  (f     --> ID               #'(lambda (ID) (list (mk-place (t-get-val ID))
-						   (mk-code nil))))
+  (f     --> ID               #'(lambda (ID) (progn 
+					       (mk-sym-entry (t-get-val ID))
+					       (list (mk-place (t-get-val ID))
+						     (mk-code nil)))))
   ))
 
 (defparameter lexforms '(ID END COLON EQLS LP RP ADD SUB MULT DIV))
